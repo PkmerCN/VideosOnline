47e9981e451f3f46d5bb1e07576b97a5da1e63bd
Q10Viking <1193094618@qq.com>
2024-05-24 00:40:47 +0800

    init maven project

---
+++ api/demo/src/main/java/org/hzz/Main.java
@@ -0,0 +1,17 @@
+package org.hzz;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner{
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
