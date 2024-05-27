dda0dbff77720aab8a91d209000b5bb55273c4e1
Q10Viking <1193094618@qq.com>
2024-05-28 00:30:19 +0800

    apipost 和 注释添加，更加专业

--- api/app-auth/auth-servier/src/main/java/org/hzz/controller/AuthController.java
+++ api/app-auth/auth-servier/src/main/java/org/hzz/controller/AuthController.java
@@ -4,6 +4,11 @@ package org.hzz.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/5/28
+ */
 @RestController
 public class AuthController {
 
