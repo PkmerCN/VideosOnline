0338c75abe9e40914b517633a42bd3c7285c7f70
zhuangzhuang-huang <q10viking@outlook.com>
2024-05-29 23:53:50 +0800

    了解payload

--- api/app-auth/auth-server/src/test/java/org/hzz/jwt/GenerateJWT.java
+++ api/app-auth/auth-server/src/test/java/org/hzz/jwt/GenerateJWT.java
@@ -5,12 +5,17 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.Base64Codec;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecureDigestAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.security.Key;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Map;
+
+import static io.jsonwebtoken.Jwts.*;
 
 /**
  * JWT练习：生成token和验证
@@ -22,12 +27,14 @@ import java.util.Map;
 @Slf4j
 public class GenerateJWT {
     String key;
-
+    long expirationTime;
     @Before
     public void beforeGenerateKey() {
         // https://www.devglan.com/online-tools/hmac-sha256-online
         // 用SHA-256 加密的SecretKey 123 输出base64格式
         key = "9cZjXN9uHH0erCgOAdZCsXIAJbeh7Q43eLa/c1a69rU=";
+        // 1h in millisecond
+        expirationTime = 3600000;
     }
 
     @Test
@@ -35,11 +42,14 @@ public class GenerateJWT {
         Map<String, ?> extraClaims = Map.of("roles", "admin",
                 "slogan", "Love Coding");
 
-        String token = Jwts.builder()
+        String token = builder()
                 .header()
                 .type("JWT").and()
+                .subject("PangKa")
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
                 .claims(extraClaims)
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .signWith(getSignInKey())
                 .compact();
         System.out.println(token);
         //三部分的base64解密
