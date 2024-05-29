8c74a43d8e671e3291cdf9fa8503f81cb9188dcc
zhuangzhuang-huang <q10viking@outlook.com>
2024-05-30 00:22:03 +0800

    添加异常捕获

--- api/app-auth/auth-server/src/test/java/org/hzz/jwt/ParseJWT.java
+++ api/app-auth/auth-server/src/test/java/org/hzz/jwt/ParseJWT.java
@@ -1,6 +1,7 @@
 package org.hzz.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -10,28 +11,34 @@ import javax.crypto.SecretKey;
 
 /**
  * 解析token
- * @see GenerateJWT
+ *
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/5/29
+ * @see GenerateJWT
  */
 public class ParseJWT {
     final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQYW5nS2EiLCJpYXQiOjE3MTY5OTc4NzYsImV4cCI6MTcxNzAwMTQ3Niwic2xvZ2FuIjoiTG92ZSBDb2RpbmciLCJyb2xlcyI6ImFkbWluIn0.RjNVFXwMjBi891xKyN_yncltUok4kvqV7ZDG_JpR_u4";
-    final String key =  "9cZjXN9uHH0erCgOAdZCsXIAJbeh7Q43eLa/c1a69rU=";
-    @Test
-    public void parse(){
-        Claims payload = Jwts.parser()
-                .verifyWith(getSignKey())
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+    final String key = "9cZjXN9uHH0erCgOAdZCsXIAJbeh7Q43eLa/c1a69rU=";
 
-        System.out.println(payload);
-        System.out.println(payload.get("slogan"));
-        System.out.println(payload.getSubject());
+    @Test
+    public void parse() {
+        try {
+            Claims payload = Jwts.parser()
+                    .verifyWith(getSignKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            System.out.println(payload);
+            System.out.println(payload.get("slogan"));
+            System.out.println(payload.getSubject());
+        } catch (JwtException ex) {
+        }
+
     }
 
-    private SecretKey getSignKey(){
+    private SecretKey getSignKey() {
         // 这里的key是A Base64-encoded string
         byte[] keyBytes = Decoders.BASE64.decode(key);
         return Keys.hmacShaKeyFor(keyBytes);
