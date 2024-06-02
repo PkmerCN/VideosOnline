8fae3b5719733df86d6c30dec4696b6ce76f45cf
zhuangzhuang-huang <1193094618@qq.com>
2024-06-02 18:00:55 +0800

    集成mybatis springboot starter

--- api/app-auth/auth-server/src/main/java/org/hzz/AuthServerCommandLiner.java
+++ api/app-auth/auth-server/src/main/java/org/hzz/AuthServerCommandLiner.java
@@ -1,6 +1,8 @@
 package org.hzz;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hzz.mapper.TestConnectionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
@@ -10,9 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthServerCommandLiner implements CommandLineRunner, Ordered {
 
+    @Autowired
+    private TestConnectionMapper testConnectionMapper;
+
     @Override
     public void run(String... args){
-        log.info("Auth Server on work2");
+        String result = testConnectionMapper.testConnection();
+        if(result != null){
+            log.info("auth-server connect {} success",result);
+        }
     }
 
     @Override
