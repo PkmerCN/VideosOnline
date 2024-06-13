31b16723c3dbde1897f44f628675b72a2cababd4
zhuangzhuang-huang <1193094618@qq.com>
2024-06-13 23:47:28 +0800

    复杂的生成

--- api/app-user/src/main/java/org/hzz/mapper/AppUserMapper.java
+++ api/app-user/src/main/java/org/hzz/mapper/AppUserMapper.java
@@ -1,17 +1,31 @@
 package org.hzz.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.hzz.domain.entity.AppUser;
+import org.hzz.domain.entity.AppUserExample;
 
 import java.util.List;
 
 public interface AppUserMapper {
+    long countByExample(AppUserExample example);
+
+    int deleteByExample(AppUserExample example);
+
     int deleteByPrimaryKey(Long id);
 
     int insert(AppUser row);
 
+    int insertSelective(AppUser row);
+
+    List<AppUser> selectByExample(AppUserExample example);
+
     AppUser selectByPrimaryKey(Long id);
 
-    List<AppUser> selectAll();
+    int updateByExampleSelective(@Param("row") AppUser row, @Param("example") AppUserExample example);
+
+    int updateByExample(@Param("row") AppUser row, @Param("example") AppUserExample example);
+
+    int updateByPrimaryKeySelective(AppUser row);
 
     int updateByPrimaryKey(AppUser row);
 }
