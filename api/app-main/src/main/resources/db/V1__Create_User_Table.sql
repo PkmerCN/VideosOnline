7f8509aa42141ff0a42eee26a6569f9b0dfd7a96
zhuangzhuang-huang <1193094618@qq.com>
2024-06-13 21:27:58 +0800

    更新用户表

---
+++ api/app-main/src/main/resources/db/V1__Create_User_Table.sql
@@ -0,0 +1,16 @@
+-- 创建用户表
+drop table if exists app_user;
+create table app_user(
+    id bigint not null auto_increment comment '主键',
+    username varchar(11) not null comment '用户名',
+    password varchar(255) not null comment '密码',
+    email varchar(30) not null comment '邮箱',
+    cell_phone varchar(11) not null comment '手机号',
+    type tinyint not null DEFAULT 0 comment '用户类型：1-员工, 2-普通学员，3-老师',
+    create_time datetime not null default CURRENT_TIMESTAMP comment '创建时间',
+    update_time datetime not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '创建时间',
+    PRIMARY KEY (`id`) USING BTREE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表' ROW_FORMAT = Dynamic;
+
+insert into app_user(username,password,email,cell_phone) value("pkmer","$2b$10$v54tBm6hSleOidxLJvznju9KZkBgv6IubYoGwKvyrUd84Nb7aLTpK","1193094618@qq.com","17801054490");
+insert into app_user(username,password,email,cell_phone) value("pangka","$2b$10$v54tBm6hSleOidxLJvznju9KZkBgv6IubYoGwKvyrUd84Nb7aLTpK","q10viking@outlook.com","17877361854");
