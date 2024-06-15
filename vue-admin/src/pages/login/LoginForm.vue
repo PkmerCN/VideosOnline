11c590aa5959ae13f0067d369222903106319272
zhuangzhuang-huang <1193094618@qq.com>
2024-06-15 19:33:35 +0800

    开始编写登录页面

--- vue-admin/src/pages/login/LoginForm.vue
+++ vue-admin/src/pages/login/LoginForm.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
-
+import bgVideo from "@/assets/videos/login.mp4"
+import LoginAndSign from './components/LoginAndSign.vue'
 </script>
 
 <template>
-<div class="container">
-  Login Form
+<div class="login-form__frame">
+  <video autoplay loop muted preload="auto"  class="bg-video">
+    <source :src="bgVideo" type="video/mp4">
+  </video>
+  <div class="login-form__container">
+    <LoginAndSign/>
+  </div>
 </div>
 </template>
 
 <style scoped lang="scss">
+.login-form__frame{
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  .bg-video{
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+  }
 
+  .login-form__container{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    width: 60%;
+    height: 55%;
+    background-color: antiquewhite;
+  }
+}
 </style>
