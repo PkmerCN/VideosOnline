421f3d0f72ed7d6e472e5ed0228fe01f73bd076c
zhuangzhuang-huang <1193094618@qq.com>
2024-06-15 19:33:35 +0800

    开始编写登录页面

---
+++ vue-admin/src/pages/login/components/LoginAndSign.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login-sign__container">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.login-sign__container{
+
+}
+</style>
