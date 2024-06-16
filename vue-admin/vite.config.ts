9743c7c91f4cdf37b9032f6bbfe723340997f396
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 20:45:46 +0800

    配置代理服务与env结合

--- vue-admin/vite.config.ts
+++ vue-admin/vite.config.ts
@@ -1,22 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig((mode)=> {
+  const {VITE_APP_API_URL: SERVER_API_URL} = loadEnv(mode.mode,process.cwd())
+  return {
+    plugins: [
+      vue(),
+      VueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server:{
+      port: 18080,
+      host: "0.0.0.0",
+      proxy:{
+        '/api':{
+          target: SERVER_API_URL,
+          changeOrigin:true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
     }
-  },
-  server:{
-    port: 18080,
-    host: "0.0.0.0"
   }
 })
