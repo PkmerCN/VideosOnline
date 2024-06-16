817449d4876c0aba4dddbb342991930cc65be4db
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 20:45:00 +0800

    增加一个axios地址的一个配置

--- vue-admin/src/utils/env-test.ts
+++ vue-admin/src/utils/env-test.ts
@@ -7,3 +7,5 @@ console.log("PROD = ",import.meta.env.PROD)
 // 访问自己定义的变量
 console.log(import.meta.env.VITE_APP_TITLE)
 console.log(import.meta.env.VITE_APP_API_URL)
+console.log("timeout = ",import.meta.env.VITE_APP_TIMEOUT)
+console.log("axios baseUrl = ",import.meta.env.VITE_APP_AXIOS_BASE_URL)
