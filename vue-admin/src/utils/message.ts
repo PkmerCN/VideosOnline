e13aa4c83f2276d163c1014f37e3b679a35b04e0
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 17:35:52 +0800

    格式化代码

--- vue-admin/src/utils/message.ts
+++ vue-admin/src/utils/message.ts
@@ -1,5 +1,5 @@
 import MsgPrompt from '@/components/message/MsgPrompt.vue'
-import {h,render} from 'vue'
+import { h, render } from 'vue'
 
 const createMessage = () => {
   const containerDiv = document.createElement('div')
@@ -7,14 +7,14 @@ const createMessage = () => {
   // 创建虚拟节点
   const vNode = h(MsgPrompt)
   // 使用render函数将VNode渲染到临时DOM容器
-  render(vNode,containerDiv)
+  render(vNode, containerDiv)
 
-  const {showMessage} =  vNode.component?.exposed as {showMessage: (msg: string, msgType?: MsgType) => void}
+  const { showMessage } = vNode.component?.exposed as { showMessage: (msg: string, msgType?: MsgType) => void }
 
   return {
-    info: (msg: string)=>showMessage(msg,'info'),
-    success: (msg: string)=>showMessage(msg,'success'),
-    error: (msg: string)=>showMessage(msg,'error')
+    info: (msg: string) => showMessage(msg, 'info'),
+    success: (msg: string) => showMessage(msg, 'success'),
+    error: (msg: string) => showMessage(msg, 'error')
   }
 }
 
