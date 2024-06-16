f525b0ba4eac0fb1f6dd74b8340dbe96231cb234
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 20:45:30 +0800

    配置

--- vue-admin/src/utils/http.ts
+++ vue-admin/src/utils/http.ts
@@ -3,17 +3,28 @@
  */
 import axios, { type AxiosRequestConfig } from 'axios'
 import type { AppHTTP } from '@/@types/app-http/http'
+import {timeout,baseURL} from '@/config/env-variables'
 
+
+const axiosInstance=axios.create({
+  baseURL,
+  timeout
+})
+
+
+/**
+ * 封装Get和POST方法
+ */
 const http: AppHTTP = {
   async get<T>(url: string, data?: Data, config?: AxiosRequestConfig): AppResponse<T> {
-    const response = await axios.get<AppBaseResponse<T>>(url, {
+    const response = await axiosInstance.get<AppBaseResponse<T>>(url, {
       params: data,
       ...config
     })
     return response.data
   },
   async post<T>(url: string, data?: Data, config?: AxiosRequestConfig): AppResponse<T> {
-    const response = await axios.post<AppBaseResponse<T>>(url, data, config);
+    const response = await axiosInstance.post<AppBaseResponse<T>>(url, data, config);
     return response.data;
   }
 };
