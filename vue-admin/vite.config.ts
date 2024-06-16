6226b5fc5d9abd23ebc2c17c1251a9a3c965f281
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 22:22:12 +0800

    rename

--- vue-admin/vite.config.ts
+++ vue-admin/vite.config.ts
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig((mode)=> {
-  const {VITE_APP_API_URL: SERVER_API_URL} = loadEnv(mode.mode,process.cwd())
+export default defineConfig((configEnv)=> {
+  const {VITE_APP_API_URL: SERVER_API_URL} = loadEnv(configEnv.mode,process.cwd())
   return {
     plugins: [
       vue(),
