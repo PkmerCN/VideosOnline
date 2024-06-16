b4915cba5d8873ab36572e7ff64878edb0ca98fb
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 16:36:24 +0800

    消息叠加

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <LoginForm/>
+    <button @click="Message.info('info')">展示info消息</button>
+    <button @click="Message.error('error')">展示error消息</button>
+    <button @click="Message.success('success')">展示success消息</button>
+<!--    <LoginForm/>-->
   </div>
 </template>
 
 <script setup lang="ts">
 import LoginForm from '@/pages/login'
+import Message from '@/utils/message'
+
 </script>
 
 <style lang="scss" scoped>
