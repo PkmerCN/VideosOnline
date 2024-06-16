4c0cf294138b2f2e22810e1a555d45ea80186125
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 14:05:57 +0800

    消息模型初步实现

---
+++ vue-admin/src/@types/app-msg/index.d.ts
@@ -0,0 +1,1 @@
+type MsgType = 'info' | 'success' | 'error'
---
+++ vue-admin/src/components/message/MsgPrompt.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const messageType = ref<MsgType>('info')
+const messageText = ref('')
+const visiable = ref(false)
+
+function showMessage(msg:string,msgType:MsgType = 'info') {
+  messageText.value = msg
+  messageType.value = msgType
+  visiable.value = true
+
+  setTimeout(() => {
+    visiable.value = false
+  },3000)
+}
+
+defineExpose({showMessage})
+</script>
+
+<template>
+<div v-if="visiable" :class="['message', messageType]">
+  {{messageText}}
+</div>
+</template>
+
+<style scoped lang="scss">
+
+.message{
+  font-size: 26px;
+}
+
+.info{
+  color:sandybrown;
+}
+.success{
+  color: green;
+}
+.error{
+  color: red;
+}
+</style>
