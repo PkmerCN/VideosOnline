8c9c13510378498e5186f1ca6a8ef18a6083b1a9
zhuangzhuang-huang <1193094618@qq.com>
2024-06-16 14:05:57 +0800

    消息模型初步实现

---
+++ vue-admin/src/utils/message.ts
@@ -0,0 +1,22 @@
+import MsgPrompt from '@/components/message/MsgPrompt.vue'
+import {h,render} from 'vue'
+
+const createMessage = () => {
+  const containerDiv = document.createElement('div')
+  document.body.appendChild(containerDiv)
+  // 创建虚拟节点
+  const vNode = h(MsgPrompt)
+  // 使用render函数将VNode渲染到临时DOM容器
+  render(vNode,containerDiv)
+
+  const {showMessage} =  vNode.component?.exposed as {showMessage: (msg: string, msgType?: MsgType) => void}
+
+  return {
+    info: (msg: string)=>showMessage(msg,'info'),
+    success: (msg: string)=>showMessage(msg,'success'),
+    error: (msg: string)=>showMessage(msg,'error')
+  }
+}
+
+const Message = createMessage()
+export default Message
