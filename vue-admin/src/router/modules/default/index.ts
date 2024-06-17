083cbd6fa4bbc0c67d14d590b99180ebd378f7b9
zhuangzhuang-huang <1193094618@qq.com>
2024-06-17 23:24:10 +0800

    更新配置

--- vue-admin/src/router/modules/default/index.ts
+++ vue-admin/src/router/modules/default/index.ts
@@ -1,12 +1,22 @@
 /**
  * 存放默认的路由
  */
-const defaultRoutes = [
+import type {RouteRecordRaw} from 'vue-router'
+
+const defaultRoutes:RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    component: () => import('@/pages/home')
+  },
   {
     path: '/login',
     name: 'login',
     component: () => import('@/pages/login')
-  }
+  },
 ]
 
 export default defaultRoutes
