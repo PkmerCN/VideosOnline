f23fc744d93fb1821d93c8e632727af1a440d143
zhuangzhuang-huang <1193094618@qq.com>
2024-06-18 00:30:46 +0800

    导航守卫修复

--- vue-admin/src/router/guards/index.ts
+++ vue-admin/src/router/guards/index.ts
@@ -1,13 +1,27 @@
 /**
  * 配置全局导航守卫
  */
-import type {Router} from 'vue-router'
-function configNavigationGuards(router: Router){
-  router.beforeEach((to,from,next)=>{
+import type { Router } from 'vue-router'
+import { useUserStore } from '@/store'
+
+function configNavigationGuards(router: Router) {
+  router.beforeEach((to, from, next) => {
+    const { isLogin } = useUserStore()
     console.log(`从 ${from.path} 导航到 ${to.path}`)
-    //允许导航
-    next()
+    console.log(`是否已经登录: ${isLogin}`)
+    if(isLogin){
+      //允许导航
+      next()
+    }else{
+      // 未登录且目标路由不是登录页面，跳转到登录页面
+      if (to.path !== '/login') {
+        next('/login')
+      } else {
+        // 允许导航到登录页面
+        next()
+      }
+    }
   })
 }
-console.log("配置全局导航路由")
+
 export default configNavigationGuards
--- vue-admin/src/store/modules/user.ts
+++ vue-admin/src/store/modules/user.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import appLocalStorage from '@/utils/appLocalStorage'
 import { TOKEN_NAME } from '@/config/global-variables'
 
@@ -7,10 +7,12 @@ export const useUserStore = defineStore('user', () => {
   const count = ref(0)
   const token = ref(appLocalStorage.get<string>(TOKEN_NAME))
 
+  const isLogin = computed(()=> token.value !== null)
+
   function setToken(tokenV: string) {
     token.value = tokenV
     appLocalStorage.set(TOKEN_NAME, tokenV)
   }
 
-  return { count, token, setToken }
+  return { count, token,isLogin, setToken }
 })
