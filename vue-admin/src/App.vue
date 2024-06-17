0f16e851397646227272b03ce8325bf92d2d8a01
zhuangzhuang-huang <1193094618@qq.com>
2024-06-17 19:35:34 +0800

    初步定义store

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div>
-<!--    <button @click="Message.info('infoxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')">展示info消息</button>-->
-<!--    <button @click="Message.error('error')">展示error消息</button>-->
-<!--    <button @click="Message.success('success')">展示success消息</button>-->
     <LoginForm/>
   </div>
 </template>
 
 <script setup lang="ts">
 import LoginForm from '@/pages/login'
-// import Message from '@/utils/message'
-
 </script>
 
 <style lang="scss" scoped>
--- vue-admin/src/main.ts
+++ vue-admin/src/main.ts
@@ -3,10 +3,10 @@ import "@/assets/style/main.scss"
 import App from './App.vue'
 import '@/utils/env-test'
 import msgPlugin from '@/plugins/msg'
-import {createPinia} from 'pinia'
+import store from '@/store'
 
 const app = createApp(App)
-const pinia = createPinia()
+
 app.use(msgPlugin)
-app.use(pinia)
+app.use(store)
 app.mount('#app')
