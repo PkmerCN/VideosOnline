33d39e6a02936c8453141d303f8e26ca46a2b29d
zhuangzhuang-huang <1193094618@qq.com>
2024-06-17 18:13:12 +0800

    引入pinia

--- vue-admin/src/main.ts
+++ vue-admin/src/main.ts
@@ -3,7 +3,10 @@ import "@/assets/style/main.scss"
 import App from './App.vue'
 import '@/utils/env-test'
 import msgPlugin from '@/plugins/msg'
+import {createPinia} from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(msgPlugin)
+app.use(pinia)
 app.mount('#app')
