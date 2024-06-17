666b974e951857cdb06592f051b11b56936e1023
zhuangzhuang-huang <1193094618@qq.com>
2024-06-17 22:15:46 +0800

    格式化代码

--- vue-admin/src/store/modules/user.ts
+++ vue-admin/src/store/modules/user.ts
@@ -1,16 +1,16 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import appLocalStorage from '@/utils/appLocalStorage'
-import {TOKEN_NAME} from '@/config/global-variables'
+import { TOKEN_NAME } from '@/config/global-variables'
 
 export const useUserStore = defineStore('user', () => {
   const count = ref(0)
   const token = ref(appLocalStorage.get<string>(TOKEN_NAME))
 
-  function setToken(tokenV:string){
+  function setToken(tokenV: string) {
     token.value = tokenV
-    appLocalStorage.set(TOKEN_NAME,tokenV)
+    appLocalStorage.set(TOKEN_NAME, tokenV)
   }
 
-  return { count,token,setToken }
+  return { count, token, setToken }
 })
