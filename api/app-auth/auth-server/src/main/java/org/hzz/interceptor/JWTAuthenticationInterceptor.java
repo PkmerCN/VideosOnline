02ce98218c777aef236e16a91779222652b651ea
zhuangzhuang-huang <1193094618@qq.com>
2024-06-18 19:44:00 +0800

    jwt解析token

--- api/app-auth/auth-server/src/main/java/org/hzz/interceptor/JWTAuthenticationInterceptor.java
+++ api/app-auth/auth-server/src/main/java/org/hzz/interceptor/JWTAuthenticationInterceptor.java
@@ -1,7 +1,15 @@
 package org.hzz.interceptor;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.hzz.domain.bo.UserBo;
+import org.hzz.exceptions.auth.AppTokenExpireException;
+import org.hzz.exceptions.auth.AppTokenInvalidException;
+import org.hzz.services.JWTService;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
@@ -9,10 +17,37 @@ import org.springframework.web.servlet.HandlerInterceptor;
  * @version 1.0.0
  * @date 2024/6/12
  */
+@Slf4j
 public class JWTAuthenticationInterceptor implements HandlerInterceptor {
+
+    private JWTService jwtService;
+
+    public JWTAuthenticationInterceptor(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        final String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bear ")) {
+            String token = header.substring(5);
+            log.info("Token = {}", token);
+            try {
+                UserBo userBo = jwtService.parseToken(token);
+            } catch (ExpiredJwtException ex) {
+                log.info("token已过期");
+                throw new AppTokenExpireException();
+            } catch (JwtException ex) {
+                log.info("token不合法");
+                throw new AppTokenInvalidException();
+            }
+
+        } else {
+            log.info("no token");
+        }
+
+
+        // todo 就是如何确认token是否过期
         return true;
     }
 }
--- api/app-auth/auth-server/src/test/java/org/hzz/jwt/ParseJWT.java
+++ api/app-auth/auth-server/src/test/java/org/hzz/jwt/ParseJWT.java
@@ -5,8 +5,8 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.junit.Assert;
 import org.junit.Test;
-
 import javax.crypto.SecretKey;
 
 /**
@@ -18,7 +18,8 @@ import javax.crypto.SecretKey;
  * @see GenerateJWT
  */
 public class ParseJWT {
-    final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQYW5nS2EiLCJpYXQiOjE3MTY5OTc4NzYsImV4cCI6MTcxNzAwMTQ3Niwic2xvZ2FuIjoiTG92ZSBDb2RpbmciLCJyb2xlcyI6ImFkbWluIn0.RjNVFXwMjBi891xKyN_yncltUok4kvqV7ZDG_JpR_u4";
+    //final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQYW5nS2EiLCJpYXQiOjE3MTY5OTc4NzYsImV4cCI6MTcxNzAwMTQ3Niwic2xvZ2FuIjoiTG92ZSBDb2RpbmciLCJyb2xlcyI6ImFkbWluIn0.RjNVFXwMjBi891xKyN_yncltUok4kvqV7ZDG_JpR_u4";
+    final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJQYW5nS2EiLCJpYXQiOjE3MTg3MDg1OTMsImV4cCI6MTcxODcwODU5Mywicm9sZXMiOiJhZG1pbiIsInNsb2dhbiI6IkxvdmUgQ29kaW5nIn0.IzVTNG57oBbHz-3UCIi5lDQi1qMYwlvN8WXZYmS5Ksg";
     final String key = "9cZjXN9uHH0erCgOAdZCsXIAJbeh7Q43eLa/c1a69rU=";
 
     @Test
@@ -37,6 +38,13 @@ public class ParseJWT {
             System.out.println(ex);
         }
 
+    }
+
+    @Test
+    public void subToken(){
+        String tmp = "Bear "+token;
+        System.out.println(tmp.substring(5));
+        Assert.assertEquals(token,tmp.substring(5));
     }
 
     private SecretKey getSignKey() {
