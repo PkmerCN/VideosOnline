546a65ae1b9b1899d1fa40129705a80598e54496
zhuangzhuang-huang <1193094618@qq.com>
2024-06-18 19:41:27 +0800

    添加builder模式

--- api/app-user/src/main/java/org/hzz/domain/bo/UserBo.java
+++ api/app-user/src/main/java/org/hzz/domain/bo/UserBo.java
@@ -1,6 +1,7 @@
 package org.hzz.domain.bo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserBo {
     private Long id;
     private String username;
