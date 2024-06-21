17dfe8e9373c8a4fa9787a71ae7aac639387d461
zhuangzhuang-huang <1193094618@qq.com>
2024-06-21 23:52:26 +0800

    完善jwt自动配置

--- api/app-framework-all/app-security-spring-boot-starter/src/main/java/org/hzz/security/config/AppSecurityAutoConfig.java
+++ api/app-framework-all/app-security-spring-boot-starter/src/main/java/org/hzz/security/config/AppSecurityAutoConfig.java
@@ -1,10 +1,12 @@
 package org.hzz.security.config;
 
+import org.hzz.security.jwt.service.JWTService;
 import org.hzz.security.password.AppPasswordEncoder;
 import org.hzz.security.password.BcryptPassword4j;
 import org.hzz.security.jwt.props.JWTProps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
@@ -15,9 +17,20 @@ import org.springframework.context.annotation.Bean;
  */
 @EnableConfigurationProperties(JWTProps.class)
 public class AppSecurityAutoConfig {
+    private final Logger LOG = LoggerFactory.getLogger(AppSecurityAutoConfig.class);
+
     @Bean
     @ConditionalOnMissingBean
     public AppPasswordEncoder bcryptPassword4j(){
+        LOG.info("自动注入AppPasswordEncoder Success");
         return new BcryptPassword4j();
     }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public JWTService jwtService(JWTProps jwtProps){
+        LOG.info("正在自动注入JWTService,使用JwtProps = {}",jwtProps.toString());
+        return new JWTService(jwtProps);
+    }
+
 }
