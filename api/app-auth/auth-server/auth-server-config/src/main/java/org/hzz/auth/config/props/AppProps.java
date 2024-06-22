b4fcf27e7f62cf418d7c62722067d2142fc9bbfc
zhuangzhuang-huang <1193094618@qq.com>
2024-06-22 23:33:09 +0800

    开始配置

--- api/app-auth/auth-server/auth-server-config/src/main/java/org/hzz/auth/config/props/AppProps.java
+++ api/app-auth/auth-server/auth-server-config/src/main/java/org/hzz/auth/config/props/AppProps.java
@@ -3,6 +3,7 @@ package org.hzz.auth.config.props;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * @author 胖卡
@@ -11,7 +12,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @Setter
 @Getter
-@ConfigurationProperties("app")
+@ConfigurationProperties(prefix = "app.auth")
 public class AppProps {
     private String[] ignore_urls;
 }
