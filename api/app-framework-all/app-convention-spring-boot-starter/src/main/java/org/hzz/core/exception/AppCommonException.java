313e0c800435d74789a0d59a6e2082fe0550111f
zhuangzhuang-huang <1193094618@qq.com>
2024-06-22 12:53:46 +0800

    常量与异常处理

---
+++ api/app-framework-all/app-convention-spring-boot-starter/src/main/java/org/hzz/core/exception/AppCommonException.java
@@ -0,0 +1,36 @@
+package org.hzz.core.exception;
+
+
+import org.hzz.core.code.AppStatus;
+import org.hzz.core.code.impl.AppStatusImpl;
+
+/**
+ * 系统异 常
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/14
+ */
+public class AppCommonException extends RuntimeException implements AppException {
+
+    private AppStatus appStatus;
+
+    public AppCommonException(){
+        super();
+        appStatus = AppStatusImpl.INTERNAL_ERROR;
+    }
+
+    public AppCommonException(String messsage){
+        super(messsage);
+        appStatus = AppStatusImpl.INTERNAL_ERROR;
+    }
+
+    public AppCommonException(AppStatus appStatus){
+        super(appStatus.getReason());
+        this.appStatus = appStatus;
+    }
+
+    @Override
+    public AppStatus getStatus() {
+        return appStatus;
+    }
+}
