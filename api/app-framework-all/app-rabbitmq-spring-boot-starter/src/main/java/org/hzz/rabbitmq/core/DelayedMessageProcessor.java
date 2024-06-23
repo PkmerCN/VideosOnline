5ca1b3b62db8cc2d160a5651f7ef581d56336f29
zhuangzhuang-huang <1193094618@qq.com>
2024-06-23 21:31:47 +0800

    延迟消息设置成功

---
+++ api/app-framework-all/app-rabbitmq-spring-boot-starter/src/main/java/org/hzz/rabbitmq/core/DelayedMessageProcessor.java
@@ -0,0 +1,28 @@
+package org.hzz.rabbitmq.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+
+/**
+ * 设置延迟时间
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/23
+ */
+@Slf4j
+public class DelayedMessageProcessor extends BasicIdMessageProcessor{
+    private final long delay;
+    public DelayedMessageProcessor(long delay){
+        this.delay = delay;
+    }
+
+    @Override
+    public Message postProcessMessage(Message message) throws AmqpException {
+        // 1. 添加消息requestid
+        super.postProcessMessage(message);
+
+        // 2.添加延迟时间
+        message.getMessageProperties().setHeader("x-delay",this.delay);
+    }
+}
