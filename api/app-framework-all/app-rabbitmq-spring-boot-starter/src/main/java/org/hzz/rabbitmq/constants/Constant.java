7ded23d6eb4e7b3e3f4451d24f3cab9385cad8a6
zhuangzhuang-huang <1193094618@qq.com>
2024-06-23 19:46:05 +0800

    配置docker rabbitmq

---
+++ api/app-framework-all/app-rabbitmq-spring-boot-starter/src/main/java/org/hzz/rabbitmq/constants/Constant.java
@@ -0,0 +1,10 @@
+package org.hzz.rabbitmq.constants;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/23
+ */
+public interface Constant {
+    String REQUEST_ID_HEADER = "requestId";
+}
---
+++ api/app-framework-all/app-rabbitmq-spring-boot-starter/src/main/java/org/hzz/rabbitmq/core/BasicIdMessageProcessor.java
@@ -0,0 +1,32 @@
+package org.hzz.rabbitmq.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+
+import java.util.UUID;
+
+import static org.hzz.rabbitmq.constants.Constant.*;
+
+/**
+ * rabbitmq发送消息之前和接受消息之后，设置requestId
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/23
+ */
+@Slf4j
+public class BasicIdMessageProcessor implements MessagePostProcessor {
+    @Override
+    public Message postProcessMessage(Message message) throws AmqpException {
+        String requestId = MDC.get(REQUEST_ID_HEADER);
+        if(requestId == null){
+            log.info("requestId为空，生成requestId");
+            requestId = UUID.randomUUID().toString();
+        }
+        log.info("requestId = {}",requestId);
+        message.getMessageProperties().setHeader(REQUEST_ID_HEADER,requestId);
+        return message;
+    }
+}
