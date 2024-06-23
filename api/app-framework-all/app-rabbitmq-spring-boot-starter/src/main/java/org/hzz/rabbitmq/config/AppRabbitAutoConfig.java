d990676c297e2de87fe4ae072975af8da4713490
zhuangzhuang-huang <1193094618@qq.com>
2024-06-23 15:59:48 +0800

    添加rabbitmq配置

---
+++ api/app-framework-all/app-rabbitmq-spring-boot-starter/src/main/java/org/hzz/rabbitmq/config/AppRabbitAutoConfig.java
@@ -0,0 +1,16 @@
+package org.hzz.rabbitmq.config;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/23
+ */
+@Slf4j
+public class AppRabbitAutoConfig {
+
+    public AppRabbitAutoConfig(){
+        log.info("检测到AppRabbitAutoConfig");
+    }
+}
