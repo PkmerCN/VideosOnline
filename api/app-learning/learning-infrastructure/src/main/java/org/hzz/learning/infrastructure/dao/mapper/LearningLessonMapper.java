6e9fa3d6c02189fc2971300ae6b42f869c6e471b
zhuangzhuang-huang <1193094618@qq.com>
2024-06-24 22:14:08 +0800

    生成相关对象

---
+++ api/app-learning/learning-infrastructure/src/main/java/org/hzz/learning/infrastructure/dao/mapper/LearningLessonMapper.java
@@ -0,0 +1,33 @@
+package org.hzz.learning.infrastructure.dao.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.hzz.learning.infrastructure.dao.entity.LearningLesson;
+import org.hzz.learning.infrastructure.dao.entity.LearningLessonExample;
+
+@Mapper
+public interface LearningLessonMapper {
+    long countByExample(LearningLessonExample example);
+
+    int deleteByExample(LearningLessonExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(LearningLesson row);
+
+    int insertSelective(LearningLesson row);
+
+    List<LearningLesson> selectByExample(LearningLessonExample example);
+
+    LearningLesson selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") LearningLesson row, @Param("example") LearningLessonExample example);
+
+    int updateByExample(@Param("row") LearningLesson row, @Param("example") LearningLessonExample example);
+
+    int updateByPrimaryKeySelective(LearningLesson row);
+
+    int updateByPrimaryKey(LearningLesson row);
+}
