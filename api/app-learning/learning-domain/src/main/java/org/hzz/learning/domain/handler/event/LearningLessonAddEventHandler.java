bef6fd4cdd3490ab08588e963021936f43e760e2
zhuangzhuang-huang <1193094618@qq.com>
2024-06-25 18:10:39 +0800

    领域事件基本模型处理完成

---
+++ api/app-learning/learning-domain/src/main/java/org/hzz/learning/domain/handler/event/LearningLessonAddEventHandler.java
@@ -0,0 +1,22 @@
+package org.hzz.learning.domain.handler.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hzz.learning.domain.event.LearningLessonAddEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 处理添加课程事件
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/25
+ */
+@Component
+@Slf4j
+public class LearningLessonAddEventHandler {
+
+    @EventListener
+    public void handleLearningLessonAddEvent(LearningLessonAddEvent event){
+        log.info("准备处理: {}",event);
+    }
+}
