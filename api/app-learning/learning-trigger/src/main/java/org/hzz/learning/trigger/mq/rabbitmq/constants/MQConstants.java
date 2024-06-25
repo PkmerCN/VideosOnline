bd262f2f2497044727a32831a18948aa5d811669
zhuangzhuang-huang <1193094618@qq.com>
2024-06-25 17:06:55 +0800

    创建常量

---
+++ api/app-learning/learning-trigger/src/main/java/org/hzz/learning/trigger/mq/rabbitmq/constants/MQConstants.java
@@ -0,0 +1,12 @@
+package org.hzz.learning.trigger.mq.rabbitmq.constants;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/25
+ */
+public interface MQConstants {
+    interface Exchange{}
+    interface Queue{}
+    interface Key{}
+}
