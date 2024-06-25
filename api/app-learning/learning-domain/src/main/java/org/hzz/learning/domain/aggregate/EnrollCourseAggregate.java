1b85dda159307b5d8e0a72c475af09b4ae06b34f
zhuangzhuang-huang <1193094618@qq.com>
2024-06-26 01:55:23 +0800

    DDD相关模型数据准备

---
+++ api/app-learning/learning-domain/src/main/java/org/hzz/learning/domain/aggregate/EnrollCourseAggregate.java
@@ -0,0 +1,20 @@
+package org.hzz.learning.domain.aggregate;
+
+import lombok.Builder;
+import lombok.Data;
+import org.hzz.ddd.core.domain.shared.AggregateRoot;
+
+import java.util.List;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/25
+ */
+@Data
+@Builder
+public class EnrollCourseAggregate implements AggregateRoot {
+    private Long id;
+    private Long userId;
+    List<Object> course;
+}
---
+++ api/app-learning/learning-domain/src/main/java/org/hzz/learning/domain/service/LearnLessonDomainService.java
@@ -0,0 +1,9 @@
+package org.hzz.learning.domain.service;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/25
+ */
+public interface LearnLessonDomainService {
+}
