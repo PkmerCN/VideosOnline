71eda09787569fedf22ade42e717eeb5c5ca2b0a
zhuangzhuang-huang <1193094618@qq.com>
2024-06-26 00:12:58 +0800

    创建目录草稿

---
+++ api/app-main/src/main/resources/db/V3.5.0__Create_course_catalogue_draft_Table.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS `course_catalogue_draft`;
+CREATE TABLE `course_catalogue_draft`  (
+   `id` bigint NOT NULL COMMENT '课程目录id',
+   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '目录名称',
+   `trailer` tinyint NOT NULL DEFAULT 0 COMMENT '是否支持试看',
+   `course_id` bigint NOT NULL COMMENT '课程id',
+   `type` tinyint NOT NULL COMMENT '目录类型1：章节，2：小节，3：测试',
+   `parent_catalogue_id` bigint NOT NULL DEFAULT 0 COMMENT '所属章节id，只有小节和测试有该值，章节没有，章节默认为0',
+   `media_id` bigint NOT NULL DEFAULT 0 COMMENT '媒资id',
+   `video_id` bigint NULL DEFAULT NULL COMMENT '视频id',
+   `video_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '视频名称',
+   `living_start_time` datetime NULL DEFAULT NULL COMMENT '直播开始时间',
+   `living_end_time` datetime NULL DEFAULT NULL COMMENT '直播结束时间',
+   `play_back` tinyint NOT NULL DEFAULT 0 COMMENT '是否支持回放',
+   `c_index` int NOT NULL DEFAULT 0 COMMENT '用于章节排序',
+   `media_duration` int NOT NULL DEFAULT 0 COMMENT '以s为单位',
+   `can_update` tinyint NOT NULL DEFAULT 1 COMMENT '是否可以更新0：不可以更新，1：可以更新',
+   `dep_id` bigint NOT NULL DEFAULT 0 COMMENT '部门id',
+   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+   `creater_id` bigint NOT NULL DEFAULT 0 COMMENT '创建人Id',
+   `updater_id` bigint NOT NULL DEFAULT 0 COMMENT '更新人Id',
+   `deleted` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除',
+   PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '目录草稿' ROW_FORMAT = Dynamic;
