93709c525172c0a7b908edc0fde21796b24b064d
zhuangzhuang-huang <1193094618@qq.com>
2024-06-26 02:44:04 +0800

    优化autowired

--- api/app-framework-all/app-convention-spring-boot-starter/src/main/java/org/hzz/core/repository/BaseRepository.java
+++ api/app-framework-all/app-convention-spring-boot-starter/src/main/java/org/hzz/core/repository/BaseRepository.java
@@ -1,5 +1,6 @@
 package org.hzz.core.repository;
 
+import lombok.Setter;
 import org.hzz.core.base.AppBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,9 @@ import org.springframework.stereotype.Repository;
  * @date 2024/6/20
  */
 public abstract class BaseRepository<M,C> extends AppBase {
-    @Autowired
+    @Setter(onMethod_ = @Autowired)
     protected M mapper;
 
-    @Autowired
+    @Setter(onMethod_ = @Autowired)
     protected C converter;
 }
