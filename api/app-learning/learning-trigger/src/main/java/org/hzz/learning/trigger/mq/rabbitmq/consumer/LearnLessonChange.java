334714a1fbd93811b600761928fc190e25610e83
zhuangzhuang-huang <1193094618@qq.com>
2024-06-25 17:38:44 +0800

    创建Rabbitmq消费者监听课程创建消息

--- api/app-learning/learning-trigger/src/main/java/org/hzz/learning/trigger/mq/rabbitmq/consumer/LearnLessonChange.java
+++ api/app-learning/learning-trigger/src/main/java/org/hzz/learning/trigger/mq/rabbitmq/consumer/LearnLessonChange.java
@@ -1,6 +1,22 @@
 package org.hzz.learning.trigger.mq.rabbitmq.consumer;
 
+import com.rabbitmq.client.Channel;
+import org.hzz.learning.domain.event.LearningLessonAddEvent;
+import org.hzz.learning.types.constant.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import static org.hzz.learning.types.constant.Constants.Queue.*;
+import static org.hzz.learning.types.constant.Constants.Key.*;
+import static org.hzz.order.types.constant.Constants.Exchange.*;
 
 /**
  * @author 胖卡
@@ -9,6 +25,26 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class LearnLessonChange {
+    private final Logger logger = LoggerFactory.getLogger(LearnLessonChange.class);
+
+    /**
+     * 课程支付成功，接收添加课程消息
+     * @param event 课程添加事件
+     * @param channel Rabbitmq channel 用于手动提交ack
+     */
+    @RabbitListener(bindings = {
+            @QueueBinding(
+                    value = @Queue( value = LEARNING_LESSON_PAY_QUEUE,durable = "true"),
+                    exchange = @Exchange(name = ORDER_EXCHANGE),
+                    key = LESSON_PAY_KEY
+            )
+    })
+    public void handlePayLesson(@Payload LearningLessonAddEvent event,
+                                Channel channel,
+                                @Header(AmqpHeaders.DELIVERY_TAG) long tag){
+        logger.info("接收到支付课程消息 AT {}",event.occurredOn());
 
 
+
+    }
 }
