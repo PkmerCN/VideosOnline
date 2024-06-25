485ffcdb33c951f11ce0972c148ead95a5b0a878
zhuangzhuang-huang <1193094618@qq.com>
2024-06-26 01:51:33 +0800

    添加mapstruct course

---
+++ api/app-course/course-domain/src/main/java/org/hzz/course/domain/entity/CourseSimpleInfoDto.java
@@ -0,0 +1,44 @@
+package org.hzz.course.domain.entity;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/26
+ */
+public class CourseSimpleInfoDto {
+    // 课程草稿id，对应正式草稿id
+    private Long id;
+
+    // 课程名称
+    private String name;
+    // 封面链接
+    private String coverUrl;
+
+    // 课程价格，单位为分
+    private Integer price;
+    // 课程状态
+    private Byte status;
+
+    // 是否是免费课程
+    private Boolean free;
+
+    // 一级课程分类id
+    private Long firstCateId;
+
+    // 二级课程分类id
+    private Long secondCateId;
+
+    // 三级课程分类id
+    private Long thirdCateId;
+
+    // 小节数量
+    private Integer sectionNum;
+
+    // 课程购买有效期结束时间
+    private LocalDateTime purchaseEndTime;
+
+    // 课程学习有效期，单位：月
+    private Integer validDuration;
+}
