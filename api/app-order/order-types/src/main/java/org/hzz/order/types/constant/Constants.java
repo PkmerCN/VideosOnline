73121856aea88de36a5d88a0c1f721f22af797b0
zhuangzhuang-huang <1193094618@qq.com>
2024-06-25 17:22:40 +0800

    创建模块order-types模块

---
+++ api/app-order/order-types/src/main/java/org/hzz/order/types/constant/Constants.java
@@ -0,0 +1,12 @@
+package org.hzz.order.types.constant;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/25
+ */
+public interface Constants {
+    interface Exchange{
+        String ORDER_EXCHANGE = "order.direct";
+    }
+}
