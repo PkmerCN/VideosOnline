4c0013586bb40131caf62738a8d74ea0b5eaee19
zhuangzhuang-huang <1193094618@qq.com>
2024-06-25 17:47:39 +0800

    优化领域事件

--- api/app-framework-all/app-ddd-framework-core/src/main/java/org/hzz/ddd/core/domain/shared/event/DomainEventBus.java
+++ api/app-framework-all/app-ddd-framework-core/src/main/java/org/hzz/ddd/core/domain/shared/event/DomainEventBus.java
@@ -1,11 +1,17 @@
 package org.hzz.ddd.core.domain.shared.event;
 
 /**
+ * 领域事件总线
+ * 子类实现用于发布领域事件
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/6/25
  */
-public interface DomainEventBus {
+public interface DomainEventBus<E extends DomainEvent> {
 
-    void publishDomainEvent(DomainEvent domainEvent);
+    /**
+     * 发布领域事件
+     * @param event
+     */
+    void publishDomainEvent(E event);
 }
