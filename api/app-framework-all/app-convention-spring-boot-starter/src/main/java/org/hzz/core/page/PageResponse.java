0a85a8be2fd6da3d1c875706e66a33fba020e094
zhuangzhuang-huang <1193094618@qq.com>
2024-06-27 17:13:11 +0800

    设计分页查询逻辑

---
+++ api/app-framework-all/app-convention-spring-boot-starter/src/main/java/org/hzz/core/page/PageResponse.java
@@ -0,0 +1,29 @@
+package org.hzz.core.page;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/27
+ */
+@Data
+@Builder
+public class PageResponse<T> {
+
+    @Schema(description = "总记录数")
+    private Integer total;
+
+    @Schema(description = "总页数")
+    private Integer totalPages;
+
+    @Schema(description = "当前页")
+    private Integer currentPageNo;
+
+    @Schema(description = "当前页数据")
+    private List<T> list;
+}
