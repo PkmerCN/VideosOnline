7a4be0e88b34c3594631aa1963fe9e1dbdec8116
zhuangzhuang-huang <1193094618@qq.com>
2024-06-27 15:56:56 +0800

    分页请求参数

---
+++ api/app-framework-all/app-convention-spring-boot-starter/src/main/java/org/hzz/core/page/PageRequest.java
@@ -0,0 +1,46 @@
+package org.hzz.core.page;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 分页请求
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/27
+ */
+@Data
+@Schema(description = "分页请求")
+public class PageRequest {
+    private static final Integer DEFAULT_PAGE_NO = 1;
+    private static final Integer DEFAULT_PAGE_SIZE = 20;
+
+    @Schema(description = "页码",example = "1")
+    @Min(value = 1,message = "页码不能小于1")
+    private Integer pageNo = DEFAULT_PAGE_NO;
+
+    @Schema(description = "每页大小",example = "20")
+    @Min(value = 1,message = "每页查询数量不能小于1")
+    private Integer pageSize = DEFAULT_PAGE_SIZE;
+
+    @Schema(description = "排序方式",example = "id")
+    private String sortBy;
+
+    @Schema(description = "是否升序",example = "true")
+    private Boolean isAsc = true;
+
+    @Schema(description = "排序字段和顺序")
+    private List<SortOrder> sortOrders;
+
+    @Data
+    public class SortOrder{
+        @Schema(description = "排序方式",example = "id")
+        private String field;
+
+        @Schema(description = "是否升序",example = "true")
+        private Boolean isAsc = true;
+    }
+}
