83dceca6edf957015db20aa495bdb7c2b797ecbf
zhuangzhuang-huang <1193094618@qq.com>
2024-06-28 04:33:28 +0800

    fastjson2 序列化枚举成功

---
+++ api/app-framework-all/app-fastjson2-spring-boot-starter/src/main/java/org/hzz/fastjson2/autoconfig/JsonMessageConverterConfigurer.java
@@ -0,0 +1,38 @@
+package org.hzz.fastjson2.autoconfig;
+
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/6/28
+ */
+@Configuration
+public class JsonMessageConverterConfigurer implements WebMvcConfigurer {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+        config.setWriterFeatures(JSONWriter.Feature.WriteMapNullValue, JSONWriter.Feature.PrettyFormat);
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+        converters.add(0, converter);
+    }
+}
--- api/app-learning/learning-application/src/main/java/org/hzz/learning/application/service/resp/LearnLessonDto.java
+++ api/app-learning/learning-application/src/main/java/org/hzz/learning/application/service/resp/LearnLessonDto.java
@@ -1,9 +1,11 @@
 package org.hzz.learning.application.service.resp;
 
+import com.alibaba.fastjson2.annotation.JSONField;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hzz.fastjson2.enums.BaseEnumWriter;
 import org.hzz.learning.domain.enums.LessonStatus;
 import org.hzz.learning.domain.enums.PlanStatus;
 
@@ -20,10 +22,15 @@ import java.time.LocalDateTime;
 public class LearnLessonDto {
     @Schema(description = "学习课程id")
     private Long id;
+
     @Schema(description = "课程状态，0-未学习，1-学习中，2-已学完，3-已失效")
+    @JSONField(serializeUsing = BaseEnumWriter.class)
     private LessonStatus status;
+
     @Schema(description = "学习计划状态，0-没有计划，1-计划进行中")
+    @JSONField(serializeUsing = BaseEnumWriter.class)
     private PlanStatus planStatus;
+
     @Schema(description = "总已学习章节数")
     private Integer learnedSections;
     @Schema(description = "课程购买时间")
