80ec979db8e066de64895cc32991f5245ddde971
zhuangzhuang-huang <1193094618@qq.com>
2024-06-29 03:26:59 +0800

    完成

--- api/app-learning/learning-infrastructure/src/main/java/org/hzz/learning/infrastructure/repository/mybatis/LearnLessonRecordRepositoryImpl.java
+++ api/app-learning/learning-infrastructure/src/main/java/org/hzz/learning/infrastructure/repository/mybatis/LearnLessonRecordRepositoryImpl.java
@@ -7,7 +7,6 @@ import org.hzz.learning.domain.repository.LearnLessonRecordRepository;
 import org.hzz.learning.infrastructure.dao.entity.record.LearningRecord;
 import org.hzz.learning.infrastructure.dao.entity.record.LearningRecordExample;
 import org.hzz.learning.infrastructure.dao.mapper.record.LearningRecordMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
