896ec16934b8675d4a78d17b7d4d550256153218
zhuangzhuang-huang <1193094618@qq.com>
2024-07-04 20:47:31 +0800

    添加获取本周开始和结束时间

--- api/app-framework-all/app-common-spring-boot-starter/src/main/java/org/hzz/common/date/DateUtil.java
+++ api/app-framework-all/app-common-spring-boot-starter/src/main/java/org/hzz/common/date/DateUtil.java
@@ -1,7 +1,9 @@
 package org.hzz.common.date;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 
 /**
@@ -19,4 +21,34 @@ public class DateUtil {
    public static Date changeLocalDateTimeToDate(LocalDateTime dateTime){
        return dateTime != null ? Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant()) : null;
    }
+
+
+    /**
+     * 返回当前时间所在周的周开始时间
+     */
+   public static LocalDateTime getStartOfWeek(){
+       LocalDateTime now = LocalDateTime.now();
+       return now.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+               .withHour(0)
+               .withMinute(0)
+               .withSecond(0);
+   }
+
+
+    /**
+     * 返回当前时间所在周的周结束时间
+     */
+   public static LocalDateTime getEndOfWeek(){
+       LocalDateTime now = LocalDateTime.now();
+       return now.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))
+               .withHour(23)
+               .withMinute(59)
+               .withSecond(59);
+   }
+
+    public static void main(String[] args) {
+        System.out.println("当前时间: " + LocalDateTime.now());
+        System.out.println("本周起始时间: " + getStartOfWeek());
+        System.out.println("本周结束时间: " + getEndOfWeek());
+    }
 }
