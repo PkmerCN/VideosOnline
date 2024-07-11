12963073925aca428d039bcb47b59006659c286d
zhuangzhuang-huang <1193094618@qq.com>
2024-07-11 15:41:17 +0800

    问答系统领域服务

---
+++ api/app-learning/learning-domain/src/main/java/org/hzz/learning/domain/service/question/InteractionQuestionDomainService.java
@@ -0,0 +1,12 @@
+package org.hzz.learning.domain.service.question;
+
+import org.hzz.ddd.core.domain.shared.service.DomainService;
+
+/**
+ * 问答系统领域服务
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/7/11
+ */
+public interface InteractionQuestionDomainService extends DomainService {
+}
