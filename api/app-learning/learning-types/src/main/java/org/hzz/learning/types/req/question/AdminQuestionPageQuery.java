5b0d1c4d2d23df65ba487ce1c1a07f9cb9229a96
zhuangzhuang-huang <1193094618@qq.com>
2024-07-13 15:23:55 +0800

    接受admin分页请求结束

---
+++ api/app-learning/learning-types/src/main/java/org/hzz/learning/types/req/question/AdminQuestionPageQuery.java
@@ -0,0 +1,34 @@
+package org.hzz.learning.types.req.question;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hzz.core.page.query.PageQuery;
+import org.hzz.fastjson2.enums.BaseEnumWriter;
+import org.hzz.learning.types.enums.question.QuestionStatus;
+
+import java.time.LocalDateTime;
+
+/**
+ * admin端的分页查询
+ * @author 胖卡
+ * @version 1.0.0
+ * @date 2024/7/13
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Schema(description = "admin端的分页查询")
+public class AdminQuestionPageQuery extends PageQuery {
+    @Schema(description = "课程名称")
+    String courseName;
+
+    @Schema(description = "问题查看状态 0（未查看）1已查看")
+    @JSONField(serializeUsing = BaseEnumWriter.class)
+    QuestionStatus status;
+    @Schema(description = "提问最小时间")
+    LocalDateTime beginTime;
+    @Schema(description = "提问最大时间")
+    LocalDateTime endTime;
+
+}
