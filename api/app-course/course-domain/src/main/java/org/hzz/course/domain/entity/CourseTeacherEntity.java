f1b93526a5bc1d11cfa98261d6b6a9cc0510a345
zhuangzhuang-huang <1193094618@qq.com>
2024-07-16 19:54:23 +0800

    完善代码

--- api/app-course/course-domain/src/main/java/org/hzz/course/domain/entity/CourseTeacherEntity.java
+++ api/app-course/course-domain/src/main/java/org/hzz/course/domain/entity/CourseTeacherEntity.java
@@ -1,11 +1,50 @@
 package org.hzz.course.domain.entity;
 
+import lombok.Data;
+import org.hzz.core.enums.delete.Deleted;
+import org.hzz.course.types.enums.teacher.IsShow;
 import org.hzz.ddd.core.domain.shared.Entity;
 
+import java.time.LocalDateTime;
+
 /**
+ * 教师实体
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/7/16
  */
+@Data
 public class CourseTeacherEntity implements Entity {
+    // 课程老师关系id
+    private Long id;
+
+    // 课程id
+    private Long courseId;
+
+    // 老师id
+    private Long teacherId;
+
+    // 用户端是否展示
+    private IsShow isShow;
+
+    // 序号
+    private Integer cIndex;
+
+    // 部门id
+    private Long depId;
+
+    // 创建时间
+    private LocalDateTime createTime;
+
+    // 更新时间
+    private LocalDateTime updateTime;
+
+    // 创建人ID
+    private Long createrId;
+
+    // 更新人ID
+    private Long updaterId;
+
+    // 逻辑删除
+    private Deleted deleted;
 }
--- api/app-course/course-types/src/main/java/org/hzz/course/types/enums/teacher/IsShow.java
+++ api/app-course/course-types/src/main/java/org/hzz/course/types/enums/teacher/IsShow.java
@@ -5,14 +5,15 @@ import org.hzz.core.enums.BaseEnum;
 import org.hzz.core.enums.BaseEnumTemplate;
 
 /**
- * 是否展示
+ * 用户端是否展示
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/7/16
  */
 @Getter
 public enum IsShow implements BaseEnum {
-
+    NO(0,"不展示"),
+    YES(1,"展示");
     private final Integer value;
     private final String desc;
 
