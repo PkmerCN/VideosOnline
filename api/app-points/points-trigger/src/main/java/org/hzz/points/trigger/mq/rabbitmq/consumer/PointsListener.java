bb12bc2d39a1a5dfe8d7e9818d764e1f730c4723
zhuangzhuang-huang <1193094618@qq.com>
2024-07-30 00:56:30 +0800

    注解类配置测试成功

--- api/app-points/points-trigger/src/main/java/org/hzz/points/trigger/mq/rabbitmq/consumer/PointsListener.java
+++ api/app-points/points-trigger/src/main/java/org/hzz/points/trigger/mq/rabbitmq/consumer/PointsListener.java
@@ -2,11 +2,9 @@ package org.hzz.points.trigger.mq.rabbitmq.consumer;
 
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
-import org.hzz.points.trigger.mq.rabbitmq.config.RabbitMqConfig;
 import org.hzz.rabbitmq.constants.rabbitmq.video.PointsMqConstants;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.context.annotation.Primary;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -22,62 +20,50 @@ import java.io.IOException;
 @Slf4j
 public class PointsListener {
 
-// todo 写笔记，对比绑定方式
-//    @RabbitListeners({
-//            @RabbitListener(bindings = {
-//                    @QueueBinding(
-//                            value = @Queue(name = PointsMqConstants.Queue.WRITE_REPLY,durable = "true"),
-//                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
-//                            key = PointsMqConstants.Key.WRITE_REPLY
-//                    )
-//            }),
-//            @RabbitListener(bindings = {
-//                    @QueueBinding(
-//                            value = @Queue(name = PointsMqConstants.Queue.SIGN_IN,durable = "true"),
-//                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
-//                            key = PointsMqConstants.Key.SIGN_IN
-//                    )
-//            }),
-//            @RabbitListener(bindings = {
-//                    @QueueBinding(
-//                            value = @Queue(name = PointsMqConstants.Queue.LEARN_SECTION,durable = "true"),
-//                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
-//                            key = PointsMqConstants.Key.LEARN_SECTION
-//                    )
-//            }),
-//            @RabbitListener(bindings = {
-//                    @QueueBinding(
-//                            value = @Queue(name = PointsMqConstants.Queue.WRITE_NOTE,durable = "true"),
-//                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
-//                            key = PointsMqConstants.Key.WRITE_NOTE
-//                    )
-//            }),
-//            @RabbitListener(bindings = {
-//                    @QueueBinding(
-//                            value = @Queue(name = PointsMqConstants.Queue.NOTE_GATHERED,durable = "true"),
-//                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
-//                            key = PointsMqConstants.Key.NOTE_GATHERED
-//                    )
-//            })
-//    })
-
-    /**
-     * 对应的queue bean {@link RabbitMqConfig}
-     */
-    @RabbitListener(queues = {
-            PointsMqConstants.Queue.WRITE_REPLY,
-            PointsMqConstants.Queue.SIGN_IN,
-            PointsMqConstants.Queue.LEARN_SECTION,
-            PointsMqConstants.Queue.WRITE_NOTE,
-            PointsMqConstants.Queue.NOTE_GATHERED,
-
+    // todo 写笔记，对比绑定方式
+    @RabbitListeners({
+            @RabbitListener(bindings = {
+                    @QueueBinding(
+                            value = @Queue(name = PointsMqConstants.Queue.WRITE_REPLY, durable = "true"),
+                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
+                            key = PointsMqConstants.Key.WRITE_REPLY
+                    )
+            }),
+            @RabbitListener(bindings = {
+                    @QueueBinding(
+                            value = @Queue(name = PointsMqConstants.Queue.SIGN_IN, durable = "true"),
+                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
+                            key = PointsMqConstants.Key.SIGN_IN
+                    )
+            }),
+            @RabbitListener(bindings = {
+                    @QueueBinding(
+                            value = @Queue(name = PointsMqConstants.Queue.LEARN_SECTION, durable = "true"),
+                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
+                            key = PointsMqConstants.Key.LEARN_SECTION
+                    )
+            }),
+            @RabbitListener(bindings = {
+                    @QueueBinding(
+                            value = @Queue(name = PointsMqConstants.Queue.WRITE_NOTE, durable = "true"),
+                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
+                            key = PointsMqConstants.Key.WRITE_NOTE
+                    )
+            }),
+            @RabbitListener(bindings = {
+                    @QueueBinding(
+                            value = @Queue(name = PointsMqConstants.Queue.NOTE_GATHERED, durable = "true"),
+                            exchange = @Exchange(PointsMqConstants.Exchange.POINTS_EXCHANGE),
+                            key = PointsMqConstants.Key.NOTE_GATHERED
+                    )
+            })
     })
     public void addPointsRecordListener(
             @Payload String msg,
             Channel channel,
             @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
 
-        log.info("收到消息: {}",msg);
-        channel.basicAck(tag,false);
+        log.info("收到消息: {}", msg);
+        channel.basicAck(tag, false);
     }
 }
