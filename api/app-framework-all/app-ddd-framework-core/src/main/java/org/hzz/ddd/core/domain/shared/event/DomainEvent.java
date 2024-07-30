b0c1c5e19de4b0a747e35d595ad44150a0e1fb41
zhuangzhuang-huang <1193094618@qq.com>
2024-07-30 11:10:36 +0800

    优化领域事件

--- api/app-framework-all/app-ddd-framework-core/src/main/java/org/hzz/ddd/core/domain/shared/event/DomainEvent.java
+++ api/app-framework-all/app-ddd-framework-core/src/main/java/org/hzz/ddd/core/domain/shared/event/DomainEvent.java
@@ -5,14 +5,11 @@ import org.hzz.ddd.core.domain.shared.event.base.BaseDomainEvent;
 import java.time.Instant;
 
 /**
- * 领域事件
+ * 领域事件 一般继承领域事件基础类,{@link BaseDomainEvent}
  * 通过定义一个通用的DomainEvent接口，可以确保所有领域事件具有一致的结构和行为。
  * 这不仅有助于代码的组织和维护，还可以使事件处理更加通用和灵活。
  * 每个具体的事件类只需实现该接口，并根据需要添加具体的业务逻辑。
- * 领域事件基础类,{@link BaseDomainEvent}
  * 具体事件有具体的{@link DomainEventHandler} 领域事件处理器
- *
- * 发布事件通过
  *
  * @author 胖卡
  * @version 1.0.0
