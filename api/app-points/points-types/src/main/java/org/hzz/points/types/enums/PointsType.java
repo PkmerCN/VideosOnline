30170d58b211bad6ac14c2b595dcd4c3d2720a09
zhuangzhuang-huang <1193094618@qq.com>
2024-07-31 10:45:12 +0800

    添加积分上限

--- api/app-points/points-types/src/main/java/org/hzz/points/types/enums/PointsType.java
+++ api/app-points/points-types/src/main/java/org/hzz/points/types/enums/PointsType.java
@@ -6,27 +6,33 @@ import org.hzz.core.enums.BaseEnumTemplate;
 
 /**
  * {@link org.hzz.points.infrastructure.dao.handler.PointsTypeHandler}
+ *
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/7/29
  */
 @Getter
 public enum PointsType implements BaseEnum<Integer> {
-    COURSE_LEARN(1,"课程学习"),
-    SIGN_EVERYDAY(2,"每日签到"),
-    QUESTION_REPLY(3,"课程问答"),
-    COURSE_NOTES(4,"课程笔记"),
-    COURSE_EVALUATION(5,"课程评价");
+    COURSE_LEARN(1, "课程学习", 50),
+    SIGN_EVERYDAY(2, "每日签到", 1),
+    QUESTION_REPLY(3, "课程问答", 20),
+    COURSE_NOTES(4, "课程笔记", 20),
+    COURSE_EVALUATION(5, "课程评价", 50);
 
     private final Integer value;
     private final String desc;
+    /**
+     * 积分上限
+     */
+    private final Integer maxPoints;
 
-    PointsType(Integer value,String desc){
+    PointsType(Integer value, String desc, Integer maxPoints) {
         this.desc = desc;
         this.value = value;
+        this.maxPoints = maxPoints;
     }
 
-    public static PointsType fromValue(Byte value){
-        return BaseEnumTemplate.fromByteValue(PointsType.class,value);
+    public static PointsType fromValue(Byte value) {
+        return BaseEnumTemplate.fromByteValue(PointsType.class, value);
     }
 }
