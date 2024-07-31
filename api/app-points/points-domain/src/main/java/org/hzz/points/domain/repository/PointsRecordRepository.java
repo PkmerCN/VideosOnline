f64cd2360558d198998e525eeca6ec511920674e
zhuangzhuang-huang <1193094618@qq.com>
2024-07-31 11:29:33 +0800

    完成积分分组查询SQL，使用mybatis dynamic sql

--- api/app-points/points-domain/src/main/java/org/hzz/points/domain/repository/PointsRecordRepository.java
+++ api/app-points/points-domain/src/main/java/org/hzz/points/domain/repository/PointsRecordRepository.java
@@ -2,6 +2,9 @@ package org.hzz.points.domain.repository;
 
 import org.hzz.points.domain.entity.PointsRecordEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * @author 胖卡
  * @version 1.0.0
@@ -10,4 +13,12 @@ import org.hzz.points.domain.entity.PointsRecordEntity;
 public interface PointsRecordRepository {
 
     int insert(PointsRecordEntity entity);
+
+    /**
+     * 查询用户在指定时间 <code>creatTime</code>当天的各种类型积分获得情况
+     * @param _userId 用户id
+     * @param localDateTime 时间
+     * @return 统计数据
+     */
+    List<PointsRecordEntity> selectUserPointsByLocalDateTime(Long _userId, LocalDateTime localDateTime);
 }
--- api/app-points/points-infrastructure/src/main/java/org/hzz/points/infrastructure/repository/mybatis/PointsRecordRepositoryImpl.java
+++ api/app-points/points-infrastructure/src/main/java/org/hzz/points/infrastructure/repository/mybatis/PointsRecordRepositoryImpl.java
@@ -1,16 +1,28 @@
 package org.hzz.points.infrastructure.repository.mybatis;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.hzz.common.date.DateUtil;
 import org.hzz.core.converter.RecordAndEntityConverter;
 import org.hzz.points.domain.entity.PointsRecordEntity;
 import org.hzz.points.domain.repository.PointsRecordRepository;
 import org.hzz.points.infrastructure.dao.mapper.points.PointsRecordDynamicMapper;
 import org.hzz.points.infrastructure.dao.mapper.points.PointsRecordMapper;
 import org.hzz.points.infrastructure.dao.model.points.PointsRecord;
+import org.hzz.points.types.constants.PointsRecordFields;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.mybatis.dynamic.sql.render.RenderingStrategies;
+import org.mybatis.dynamic.sql.render.RenderingStrategy;
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hzz.points.infrastructure.dao.mapper.points.PointsRecordDynamicSqlSupport.*;
+import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 /**
  * @author 胖卡
@@ -18,6 +30,7 @@ import org.springframework.stereotype.Repository;
  * @date 2024/7/30
  */
 @Repository
+@Slf4j
 public class PointsRecordRepositoryImpl implements PointsRecordRepository {
 
     @Setter(onMethod_ = @Autowired)
@@ -31,8 +44,36 @@ public class PointsRecordRepositoryImpl implements PointsRecordRepository {
         return mapper.insertSelective(record);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param _userId       用户id
+     * @param localDateTime 时间
+     * @return 数据统计情况
+     */
+    @Override
+    public List<PointsRecordEntity> selectUserPointsByLocalDateTime(Long _userId, LocalDateTime localDateTime) {
+        /**
+         * 这里我们使用的是mybatis dynamic sql
+         * where condition
+         * https://mybatis.org/mybatis-dynamic-sql/docs/conditions.html
+         */
+
+        SelectStatementProvider selectSql = select(type, sum(points).as(PointsRecordFields.POINTS))
+                .from(pointsRecord)
+                .where(userId, isEqualTo(_userId))
+                .and(createTime, isBetween(DateUtil.getStartOfDay(localDateTime)).and(DateUtil.getEndOfDay(localDateTime)))
+                .groupBy(type)
+                .build().render(RenderingStrategies.MYBATIS3);
+
+        log.info("执行SQL: {}",selectSql.getSelectStatement());
+
+        List<PointsRecord> pointsRecords = dynamicMapper.selectMany(selectSql);
+        return Converter.INSTANCE.toEntities(pointsRecords);
+    }
+
     @Mapper
-    interface Converter extends RecordAndEntityConverter<PointsRecord,PointsRecordEntity>{
+    interface Converter extends RecordAndEntityConverter<PointsRecord, PointsRecordEntity> {
         Converter INSTANCE = Mappers.getMapper(Converter.class);
     }
 }
