55392ab92aff02d473a99b3273f1a707117f45c1
zhuangzhuang-huang <1193094618@qq.com>
2024-08-03 22:45:26 +0800

    完成查询当前赛季积分排行榜

--- api/app-points/points-domain/src/main/java/org/hzz/points/domain/repository/PointsBoardCurrentRepository.java
+++ api/app-points/points-domain/src/main/java/org/hzz/points/domain/repository/PointsBoardCurrentRepository.java
@@ -1,7 +1,10 @@
 package org.hzz.points.domain.repository;
 
+import org.hzz.core.page.query.PageQuery;
 import org.hzz.points.domain.entity.PointsBoardEntity;
 import org.hzz.points.domain.entity.PointsRecordEntity;
+
+import java.util.List;
 
 /**
  * 积分排行榜repository
@@ -14,6 +17,11 @@ public interface PointsBoardCurrentRepository {
      * 查询用户当前积分排名
      */
     PointsBoardEntity queryUserCurrentPointsBoard(Long userId);
+
+    /**
+     * 查询当前积分排行榜
+     */
+    List<PointsBoardEntity> queryCurrentPointsBoardList(PageQuery pageQuery);
 
     /**
      * 增加用户当前赛季积分
--- api/app-points/points-infrastructure/src/main/java/org/hzz/points/infrastructure/repository/redis/PointsBoardCurrentRepositoryImpl.java
+++ api/app-points/points-infrastructure/src/main/java/org/hzz/points/infrastructure/repository/redis/PointsBoardCurrentRepositoryImpl.java
@@ -4,6 +4,8 @@ import cn.hutool.core.util.StrUtil;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.hzz.common.date.DateUtil;
+import org.hzz.core.exception.request.BadRequestException;
+import org.hzz.core.page.query.PageQuery;
 import org.hzz.points.domain.entity.PointsBoardEntity;
 import org.hzz.points.domain.entity.PointsRecordEntity;
 import org.hzz.points.domain.repository.PointsBoardCurrentRepository;
@@ -11,13 +13,18 @@ import org.hzz.points.types.constants.RedisConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundZSetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 使用redis来实现
  * boards::202408 001  100
+ *
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/8/3
@@ -42,10 +49,42 @@ public class PointsBoardCurrentRepositoryImpl implements PointsBoardCurrentRepos
         Long rank = ops.reverseRank(userId);
 
         PointsBoardEntity entity = new PointsBoardEntity();
-        entity.setPoints(score == null? 0: score.intValue());
+        entity.setPoints(score == null ? 0 : score.intValue());
         // redis中的rank是以0开始的
-        entity.setRank(rank == null ? 0: (byte)(rank.intValue() + 1));
+        entity.setRank(rank == null ? 0 : (byte) (rank.intValue() + 1));
         return entity;
+    }
+
+    @Override
+    public List<PointsBoardEntity> queryCurrentPointsBoardList(PageQuery pageQuery) {
+
+        long start = pageQuery.getPageNo() - 1;
+        long end = start + pageQuery.getPageSize() - 1;
+
+        if(end >= 100){
+            throw new BadRequestException("只支持查询100名以内的排名");
+        }
+
+        Set<ZSetOperations.TypedTuple<String>> pointsBoardTuples = redisTemplate.opsForZSet()
+                .reverseRangeWithScores(
+                        buildKey(LocalDateTime.now()),
+                        start,
+                        end
+                );
+
+        log.info("查询到{}条数据",pointsBoardTuples.size());
+
+        List<PointsBoardEntity> result = new ArrayList<>();
+        byte rank = (byte)(start + 1);
+        for(ZSetOperations.TypedTuple<String> tuple: pointsBoardTuples){
+            PointsBoardEntity entity = new PointsBoardEntity();
+            entity.setUserId(Long.parseLong(tuple.getValue()));
+            entity.setPoints(tuple.getScore().intValue());
+            entity.setRank(rank++);
+            result.add(entity);
+        }
+
+        return result;
     }
 
     /**
@@ -68,10 +107,10 @@ public class PointsBoardCurrentRepositoryImpl implements PointsBoardCurrentRepos
     /**
      * 构建key
      */
-    private String buildKey(LocalDateTime dateTime){
-       String key = StrUtil.format(RedisConstants.BOARDS_TEMPLATE,
+    private String buildKey(LocalDateTime dateTime) {
+        String key = StrUtil.format(RedisConstants.BOARDS_TEMPLATE,
                 dateTime.format(DateUtil.getMonthFormatCompact()));
-       log.info("当前key = {}",key);
-       return key;
+        log.info("当前key = {}", key);
+        return key;
     }
 }
