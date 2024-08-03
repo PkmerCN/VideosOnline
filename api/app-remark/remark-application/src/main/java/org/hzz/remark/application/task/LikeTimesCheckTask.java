2b32b701edaed844a4dc6187556fa342958abb9d
zhuangzhuang-huang <1193094618@qq.com>
2024-08-03 13:38:39 +0800

    优化代码

--- api/app-remark/remark-application/src/main/java/org/hzz/remark/application/task/LikeTimesCheckTask.java
+++ api/app-remark/remark-application/src/main/java/org/hzz/remark/application/task/LikeTimesCheckTask.java
@@ -4,23 +4,18 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 import jakarta.annotation.PostConstruct;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.hzz.remark.domain.service.LikedRecordDomainService;
 import org.hzz.remark.domain.service.impl.LikedRecordRedisDomainServiceImpl;
 import org.hzz.remark.types.BizType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 定时任务，处理redis中的likes点赞总数
+ *
  * @author 胖卡
  * @version 1.0.0
  * @date 2024/7/27
@@ -39,26 +34,26 @@ public class LikeTimesCheckTask {
     private Long count;
 
     @PostConstruct
-    public void init(){
+    public void init() {
         bizTypes.addAll(getBizType());
         count = 30L;
     }
 
-//    @Scheduled(fixedDelay = 3,timeUnit = TimeUnit.SECONDS)
+    //    @Scheduled(fixedDelay = 3,timeUnit = TimeUnit.SECONDS)
     @XxlJob("handleLikesToDB")
-    public void test(){
+    public void test() {
         log.info("准备处理业务的点赞总数");
-        for(String bizType: bizTypes){
-            domainService.readLikedTimesAndSendMsg(bizType,count);
+        for (String bizType : bizTypes) {
+            domainService.readLikedTimesAndSendMsg(bizType, count);
         }
     }
 
     /**
      * 获取当前业务类型
      */
-    public List<String> getBizType(){
+    public List<String> getBizType() {
         List<String> results = new ArrayList<>();
-        for(BizType bizType: BizType.values()){
+        for (BizType bizType : BizType.values()) {
             String value = bizType.getValue();
             results.add(value);
         }
